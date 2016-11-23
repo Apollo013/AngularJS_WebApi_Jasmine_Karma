0396ff4fd5ebc3cf34e3e2bb38eb04b0bc57d398
Paul <paul.j.millar.1971@gmail.com>
2016-11-23 08:08:34 +0000

    Added Caching, cookies & filtering examples

---
+++ Refresher/app/js/Controllers/CookiesController.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+eventsApp.controller('CookiesController', ['$scope', '$cookies',
+    function ($scope, $cookies) {
+
+        $scope.event = { id: 1, name: 'New Event' };
+
+        $scope.saveCookie = function (event) {
+            $cookies.putObject("event", event, { secure: false, path:"/" });
+        }
+
+        $scope.getCookie = function () {
+            console.log('getting');
+            var ev = $cookies.getObject("event", { path: "/" });
+            console.log(ev.name);
+        }
+
+        $scope.removeCookie = function () {
+            console.log('removing');
+            $cookies.remove("event", { path: "/" });
+        }
+    }
+])
---
+++ Refresher/app/js/Controllers/FilterSampleController.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+eventsApp.controller('FilterSampleController', ['$scope', '$filter', 'durationsFilter',
+    function ($scope, $filter, durationsFilter) {
+        $scope.data = {};
+
+        var durations = $filter('durations');
+
+        $scope.data.duration1 = durations(1);
+        $scope.data.duration2 = durations(2);
+        $scope.data.duration3 = durations(3);
+        $scope.data.duration4 = durations(4);
+
+        $scope.data.duration5 = durationsFilter(1);
+        $scope.data.duration6 = durationsFilter(2);
+        $scope.data.duration7 = durationsFilter(3);
+        $scope.data.duration8 = durationsFilter(4);
+
+    }
+])
